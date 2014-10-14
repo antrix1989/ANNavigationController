20e9f5b3b451c9463a84de18b09207f214b66e1b
Sergey Demchenko <antrix1989@gmail.com>
2014-10-14 14:26:35 +0300

    Disabled intercative pop.

--- ANNavigationController.m
+++ ANNavigationController.m
@@ -23,6 +23,90 @@
     _allowNextAction = YES;
     
     self.delegate = self;
+//
+//  ANNavigationController.m
+//  Antrix
+//
+//  Created by Sergey Demchenko on 10/4/13.
+//  Copyright (c) 2014 antrix1989@gmai.com. All rights reserved.
+//
+
+#import "ANNavigationController.h"
+
+@interface ANNavigationController () <UINavigationControllerDelegate>
+
+@property (assign, nonatomic) BOOL allowNextAction;
+
+@end
+
+@implementation ANNavigationController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _allowNextAction = YES;
+    
+    self.delegate = self;
+    
+    self.interactivePopGestureRecognizer.enabled = NO;
+}
+
+- (void)setAllowNextAction:(BOOL)allowNextPop
+{
+    _allowNextAction = allowNextPop;
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    if (!self.allowNextAction && animated) {
+        return;
+    }
+    
+    self.allowNextAction = NO;
+    
+    [super pushViewController:viewController animated:animated];
+}
+
+- (UIViewController *)popViewControllerAnimated:(BOOL)animated
+{
+    if (!self.allowNextAction && animated) {
+        return nil;
+    }
+    
+    self.allowNextAction = NO;
+    UIViewController *popedViewController = [super popViewControllerAnimated:animated];
+    
+    return popedViewController;
+}
+
+- (NSArray *)popToRootViewControllerAnimated:(BOOL)animated
+{
+    if (!self.allowNextAction && animated) {
+        return nil;
+    }
+    
+    NSArray* result = [super popToRootViewControllerAnimated:animated];
+    
+    return result;
+}
+
+- (NSArray *)popToViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    NSArray *result = [super popToViewController:viewController animated:animated];
+    
+    return result;
+}
+
+#pragma mark - UINavigationControllerDelegate
+
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    self.allowNextAction = YES;
+}
+
+@end
+
 }
 
 - (void)setAllowNextAction:(BOOL)allowNextPop
